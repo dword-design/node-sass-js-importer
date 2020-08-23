b5ab2ae7fa061e406ea832af0726c2b55a56889c
dword <sebastianlandw@gmail.com>
2020-08-23 13:20:22 +0000

    fix: upgrades

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import jsontosass from 'jsontosass'
-import resolveFrom from 'resolve-from'
 import P from 'path'
+import resolveFrom from 'resolve-from'
 
 export default (url, prev) => {
-  const path = resolveFrom.silent(P.dirname(prev), url[0] === '~' ? url.substr(1) : `./${url}`)
+  const path = resolveFrom.silent(
+    P.dirname(prev),
+    url[0] === '~' ? url.substr(1) : `./${url}`
+  )
   if (path !== undefined) {
     const extension = P.extname(path)
     if (['.js', '.json'].includes(extension)) {
-      return { contents: path |> require |> JSON.stringify |> jsontosass.convert }
+      return {
+        contents: path |> require |> JSON.stringify |> jsontosass.convert,
+      }
     }
   }
   return null
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,52 +1,68 @@
-import execa from 'execa'
-import withLocalTmpDir from 'with-local-tmp-dir'
-import outputFiles from 'output-files'
-import { readFile } from 'fs-extra'
-import P from 'path'
 import { endent } from '@dword-design/functions'
+import execa from 'execa'
+import { readFile } from 'fs-extra'
+import outputFiles from 'output-files'
+import P from 'path'
+import withLocalTmpDir from 'with-local-tmp-dir'
 
 export default {
-  contrib: () => withLocalTmpDir(async () => {
-    await outputFiles({
-      'node_modules/bar/index.js': 'module.exports = { color: \'red\' }',
-      'package.json': JSON.stringify({
-        dependencies: {
-          'bar': '^1.0.0',
-        },
-      }),
-      'src/index.scss': endent`
+  contrib: () =>
+    withLocalTmpDir(async () => {
+      await outputFiles({
+        'node_modules/bar/index.js': "module.exports = { color: 'red' }",
+        'package.json': JSON.stringify({
+          dependencies: {
+            bar: '^1.0.0',
+          },
+        }),
+        'src/index.scss': endent`
         @import '~bar';
 
         body {
           background: $color;
         }
       `,
-    })
-    await execa('node-sass', ['--output', 'dist', '--importer', require.resolve('.'), 'src'])
-    expect(await readFile(P.resolve('dist', 'index.css'), 'utf8')).toEqual(endent`
+      })
+      await execa('node-sass', [
+        '--output',
+        'dist',
+        '--importer',
+        require.resolve('.'),
+        'src',
+      ])
+      expect(await readFile(P.resolve('dist', 'index.css'), 'utf8'))
+        .toEqual(endent`
       body {
         background: red; }
 
     `)
-  }),
-  local: () => withLocalTmpDir(async () => {
-    await outputFiles({
-      src: {
-        'bar.js': 'module.exports = { color: \'red\' }',
-        'index.scss': endent`
+    }),
+  local: () =>
+    withLocalTmpDir(async () => {
+      await outputFiles({
+        src: {
+          'bar.js': "module.exports = { color: 'red' }",
+          'index.scss': endent`
           @import 'bar.js';
 
           body {
             background: $color;
           }
         `,
-      },
-    })
-    await execa('node-sass', ['--output', 'dist', '--importer', require.resolve('.'), 'src'])
-    expect(await readFile(P.resolve('dist', 'index.css'), 'utf8')).toEqual(endent`
+        },
+      })
+      await execa('node-sass', [
+        '--output',
+        'dist',
+        '--importer',
+        require.resolve('.'),
+        'src',
+      ])
+      expect(await readFile(P.resolve('dist', 'index.css'), 'utf8'))
+        .toEqual(endent`
       body {
         background: red; }
 
     `)
-  }),
+    }),
 }
